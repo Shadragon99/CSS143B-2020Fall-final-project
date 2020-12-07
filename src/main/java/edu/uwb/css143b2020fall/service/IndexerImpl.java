a79c5c083d2779ca5097d1f9da17a7ab82890b53
Haaris Nadeem <haaris2001@gmail.com>
2020-12-07 22:12:51 +0500

    Progress made on IndexerImpl, having issues with List<List<Integer>> part, trying to solve this issue

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class IndexerImpl implements Indexer {
 
     public static void main(String[] args) {
+        Map<String, List<List<Integer>>> indexes = new HashMap<>();
         List<String> docs = new ArrayList<String>();
         docs.add("say hello world like you mean hello world not just world");
         docs.add("hello world");
@@ -17,56 +18,70 @@ public class IndexerImpl implements Indexer {
         List<String> names = new ArrayList<>();
         List<List<Integer>> table = new ArrayList<>();
         List<Integer> t = new ArrayList<>();
-        t.add(0);
-        for (int i = 0; i < docs.size(); i++)
-        {   int a = 0;
+        //t.add(0);
+        for (int i = 0; i < docs.size(); i++) {
+            //int a = 0;
             words = docs.get(i).split("\\s+");
             for (int x = 0; x < words.length; x++) {
 
-                table.add(t);
-                System.out.println(table + "+ 1");
+                //table.add(t);
+                //System.out.println(table + "+ 1");
                 boolean condition = false;
 
-                for (int y = 0; y < names.size(); y++)
-                {
+                for (int y = 0; y < names.size(); y++) {
                     if (words[x].equals(names.get(y))) {
                         condition = true;
-                        a++;
-                        table.get(a).add(x);
-                        System.out.println(table + "+ 2");
+                        //a++;
+                        //table.get(a).add(x);
+                        //System.out.println(table + "+ 2");
                         break;
                     }
                 }
                 if (condition == false) {
-                    table.get(a).add(x);
-                    System.out.println(table + "+ 3");
+                    //table.get(a).add(x);
+                    //System.out.println(table + "+ 3");
                     names.add(words[x]);
                 }
             }
         }
-        for (int c = 0; c < names.size(); c++){
-            table.get(c).remove(0);
+        List<Integer>[] arr = new ArrayList[docs.size() * names.size()];
+        for (int i = 0; i < (docs.size() * names.size()); i++) {
+            arr[i] = new ArrayList<Integer>();
         }
+        System.out.println(Arrays.toString(arr));
         System.out.println(names);
-        System.out.println(table);
-        /*
-        for(int c = 0; c < docs.size(); c++){
-            words = docs.get(c).split("\\s+");
-            for(int k = 0; k < names.size(); k++){
-                for(int l = 0; l < words.length; l++){
-                    if (names.get(k).equals(words[l])){
-                        table.add(null);
+        System.out.println(table + "before");
+        for(int n = 0; n < names.size(); n++){
+            for(int d = 0; d < docs.size(); d++){
+                words = docs.get(d).split("\\s+");
+                for (int a = 0; a < arr.length; a++) {
+                    for (int w = 0; w < words.length; w++) {
+                        System.out.println(names.get(d) + " current name");
+                        System.out.println(words[w] + " current word");
+                        System.out.println("");
+                        if (names.get(n).equals(words[w])) {
+                            arr[a].add(w);
+                            System.out.println(arr[a] + " added");
+                            System.out.println(table + " table");
+                            System.out.println(" ^ in if statement");
+
+                        }
+
                     }
+                    table.add(arr[a]);
+                    indexes.put(names.get(n), table);
+                    System.out.println(table);
+                    System.out.println(d +" is d");
+                    System.out.println(t + " cleared");
+                    System.out.println(indexes + "hashmap");
                 }
             }
         }
-
-         */
     }
 
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+        // indexes.put(names.get(i), table.get(i))
         List<List<Integer>> table = new ArrayList<>();
         String[] words = new String[docs.size()];
         int[] position = new int[docs.size()];
