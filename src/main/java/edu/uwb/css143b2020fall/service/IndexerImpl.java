20f6bbb3c5a279f285ad6be1bc58395b73a74887
Haaris Nadeem <haaris2001@gmail.com>
2020-12-08 18:02:38 +0500

    Progress made on IndexerImpl, self testing works correctly, adding all code actual index method

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -44,38 +44,40 @@ public class IndexerImpl implements Indexer {
                 }
             }
         }
+        /*
         List<Integer>[] arr = new ArrayList[docs.size() * names.size()];
         for (int i = 0; i < (docs.size() * names.size()); i++) {
             arr[i] = new ArrayList<Integer>();
         }
-        System.out.println(Arrays.toString(arr));
+        */
+
         System.out.println(names);
         System.out.println(table + "before");
         for(int n = 0; n < names.size(); n++){
             for(int d = 0; d < docs.size(); d++){
                 words = docs.get(d).split("\\s+");
-                for (int a = 0; a < arr.length; a++) {
                     for (int w = 0; w < words.length; w++) {
-                        System.out.println(names.get(d) + " current name");
+                        System.out.println(names.get(n) + " current name");
                         System.out.println(words[w] + " current word");
                         System.out.println("");
                         if (names.get(n).equals(words[w])) {
-                            arr[a].add(w);
-                            System.out.println(arr[a] + " added");
-                            System.out.println(table + " table");
+                            t.add(w);
+                            System.out.println(t + " this is current t");
                             System.out.println(" ^ in if statement");
 
                         }
 
                     }
-                    table.add(arr[a]);
-                    indexes.put(names.get(n), table);
+                    table.add(new ArrayList<Integer>(t));
+                    t.clear();
                     System.out.println(table);
+                    System.out.println("t is cleared");
                     System.out.println(d +" is d");
-                    System.out.println(t + " cleared");
-                    System.out.println(indexes + "hashmap");
-                }
             }
+            indexes.put(names.get(n), new ArrayList<List<Integer>>(table));
+            table.clear();
+            System.out.println("table is clear");
+            System.out.println(indexes + "hashmap");
         }
     }
 
